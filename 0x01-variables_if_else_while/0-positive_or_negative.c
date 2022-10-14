8d98e5d7333d58647c51491002b395f784c15f21
oim1999 <allafirst1@gmail.com>
2022-10-14 09:38:11 -0700

    ok

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,7 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 int main(void)
-{
-	int n;    
+{	int n;    
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)  
