7a867a5cb059a84a32c8b800c1729d8de426e602
oim1999 <allafirst1@gmail.com>
2022-10-14 09:18:55 -0700

    betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,9 @@
 
 int main(void)
 {
-	int n;
-     
+	int n;    
 	srand(time(0));
-	
 	n = rand() - RAND_MAX / 2;
-
 	if (n == 0)
 	{
 	  printf("%d is zero\n", n);
@@ -29,5 +26,4 @@ int main(void)
 	  printf("%d is negative\n", n);
           }
 	return (0);
-	
 }
