19f0963c141e9a903fe2eb34aed16f06189fee55
oim1999 <allafirst1@gmail.com>
2022-10-14 09:08:09 -0700

    fix the%d to %s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,13 +9,9 @@
 /* betty style doc for function main goes there */
 
 int main(void)
-
 {
-
 	int n;
-	
-
-
+     
 	srand(time(0));
 	
 	n = rand() - RAND_MAX / 2;
@@ -26,11 +22,12 @@ int main(void)
 	}
 	else if (n > 0)
 	{
-	  printf("%d is positive",n);
+	  printf("%d is positive", n);
 	}
-	else {
-	  printf("%d is negative", n );
-	     }
+	else
+	  {
+	  printf("%d is negative", n);
+          }
 	return (0);
 	
 }
